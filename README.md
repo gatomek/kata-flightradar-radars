9008e430f4481701b96d53910be35d3db075a566
Gatomek <tomasz.galinski@gmail.com>
2026-04-13 23:28:24 +0200

    review

---
+++ README.md
@@ -0,0 +1,2 @@
+# Remarks
+* Vite 8 (2026-03-12) requires node in version 22.12 and above (https://vite.dev/blog/announcing-vite8#node-js-support)
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import {GeoJSON, MapContainer, TileLayer} from 'react-leaflet'
 import './App.css'
 import 'leaflet/dist/leaflet.css';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
 import {computeDestinationPoint} from 'geolib';
 import type {Feature, FeatureCollection, Position} from 'geojson';
 import L, {type LatLng, type LatLngLiteral} from "leaflet";
 import hash from 'object-hash';
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+});
 
 const center: LatLngLiteral = {lat: 52.162, lng: 20.96};
 const distance = 250 * 1852; // in meters
