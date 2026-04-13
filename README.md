0ea024741a92003d47e8e1dca5bb117759531019
Gatomek <37158592+gatomek@users.noreply.github.com>
2026-04-14 00:06:17 +0200

    init (#1)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# kata-flightradar-radars
-Kata project for european radars
+# Remarks
+* Vite 8 (2026-03-12) requires node in version 22.12 and above (https://vite.dev/blog/announcing-vite8#node-js-support)
+
+# Links
+* https://github.com/vitejs/vite-plugin-vue/discussions/104
---
+++ src/App.tsx
@@ -0,0 +1,96 @@
+import {GeoJSON, MapContainer, TileLayer} from 'react-leaflet';
+import './App.css';
+import 'leaflet/dist/leaflet.css';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+import {computeDestinationPoint} from 'geolib';
+import type {Feature, FeatureCollection, Position} from 'geojson';
+import L, {type LatLng, type LatLngLiteral} from 'leaflet';
+import hash from 'object-hash';
+
+// This is the workaround of workaround for dev because leaflet have a bad concatenation
+if (import.meta.env.DEV) {
+    L.Icon.Default.imagePath = '';
+}
+
+L.Icon.Default.mergeOptions({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow
+});
+
+const center: LatLngLiteral = {lat: 52.162, lng: 20.96};
+const distance = 250 * 1852; // in meters
+
+const britain: LatLngLiteral = {lat: 52, lng: -5};
+const germany: LatLngLiteral = {lat: 52, lng: 7};
+const poland: LatLngLiteral = {lat: 52, lng: 19};
+
+const bayOfBiscay: LatLngLiteral = {lat: 46, lng: -9};
+const france: LatLngLiteral = {lat: 46, lng: 2};
+const italy: LatLngLiteral = {lat: 46, lng: 13};
+const romania: LatLngLiteral = {lat: 46, lng: 24};
+
+const coordList: LatLngLiteral[] = [britain, germany, poland, bayOfBiscay, france, italy, romania];
+
+const getRadarRanges = (coords: LatLngLiteral, distance: number): FeatureCollection => {
+    const radar: Feature = {
+        type: 'Feature',
+        properties: {
+            radar: true
+        },
+        geometry: {
+            type: 'Point',
+            coordinates: [coords.lng, coords.lat]
+        }
+    };
+
+    const points: Position[] = [];
+    for (let bearing = 0; bearing <= 360; bearing++) {
+        const destination = computeDestinationPoint(coords, distance, bearing);
+        points.push([destination.longitude, destination.latitude]);
+    }
+
+    const radarRange: Feature = {
+        type: 'Feature',
+        properties: {},
+        geometry: {
+            type: 'LineString',
+            coordinates: points
+        }
+    };
+
+    return {
+        type: 'FeatureCollection',
+        features: [radar, radarRange]
+    };
+};
+
+const pointToLayer = (feature: Feature, latLng: LatLng) => {
+    const props = feature.properties;
+    return props?.radar === true
+        ? L.marker(latLng)
+        : L.circleMarker(latLng, {
+              radius: 1
+          });
+};
+
+const geoJsonData: FeatureCollection[] = coordList.map((coords) => getRadarRanges(coords, distance));
+
+const geoData = geoJsonData.map((d) => <GeoJSON key={hash(d)} data={d} pointToLayer={pointToLayer} />);
+
+function App() {
+    return (
+        <MapContainer center={center} zoom={5} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://carto.com/attributions">CARTO</a>'
+                url="https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png"
+            />
+            {geoData}
+        </MapContainer>
+    );
+}
+
+export default App;
