501060cbbd1023af2092ad32d50a68c0c0c5ee0a
Gatomek <tomasz.galinski@gmail.com>
2026-04-14 00:05:17 +0200

    review

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import {GeoJSON, MapContainer, TileLayer} from 'react-leaflet'
-import './App.css'
+import {GeoJSON, MapContainer, TileLayer} from 'react-leaflet';
+import './App.css';
 import 'leaflet/dist/leaflet.css';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
@@ -7,18 +7,18 @@ import markerShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import {computeDestinationPoint} from 'geolib';
 import type {Feature, FeatureCollection, Position} from 'geojson';
-import L, {type LatLng, type LatLngLiteral} from "leaflet";
+import L, {type LatLng, type LatLngLiteral} from 'leaflet';
 import hash from 'object-hash';
 
 // This is the workaround of workaround for dev because leaflet have a bad concatenation
 if (import.meta.env.DEV) {
-    L.Icon.Default.imagePath = "";
+    L.Icon.Default.imagePath = '';
 }
 
 L.Icon.Default.mergeOptions({
     iconUrl: markerIcon,
     iconRetinaUrl: markerIcon2x,
-    shadowUrl: markerShadow,
+    shadowUrl: markerShadow
 });
 
 const center: LatLngLiteral = {lat: 52.162, lng: 20.96};
@@ -33,67 +33,53 @@ const france: LatLngLiteral = {lat: 46, lng: 2};
 const italy: LatLngLiteral = {lat: 46, lng: 13};
 const romania: LatLngLiteral = {lat: 46, lng: 24};
 
-const coordList: LatLngLiteral[] = [
-    britain,
-    germany,
-    poland,
-
-    bayOfBiscay,
-    france,
-    italy,
-    romania
-];
+const coordList: LatLngLiteral[] = [britain, germany, poland, bayOfBiscay, france, italy, romania];
 
 const getRadarRanges = (coords: LatLngLiteral, distance: number): FeatureCollection => {
     const radar: Feature = {
-        type: "Feature",
+        type: 'Feature',
         properties: {
             radar: true
         },
         geometry: {
-            type: "Point",
+            type: 'Point',
             coordinates: [coords.lng, coords.lat]
         }
     };
 
-    const points: Position[] = []
+    const points: Position[] = [];
     for (let bearing = 0; bearing <= 360; bearing++) {
         const destination = computeDestinationPoint(coords, distance, bearing);
         points.push([destination.longitude, destination.latitude]);
     }
 
     const radarRange: Feature = {
-        type: "Feature",
+        type: 'Feature',
         properties: {},
         geometry: {
-            type: "LineString",
+            type: 'LineString',
             coordinates: points
         }
-    }
+    };
 
     return {
-        type: "FeatureCollection",
-        features: [
-            radar,
-            radarRange
-        ]
+        type: 'FeatureCollection',
+        features: [radar, radarRange]
     };
-}
+};
 
 const pointToLayer = (feature: Feature, latLng: LatLng) => {
     const props = feature.properties;
-    return props?.radar === true ?
-        L.marker(latLng)
-        :
-        L.circleMarker(latLng, {
-            radius: 1
-        });
+    return props?.radar === true
+        ? L.marker(latLng)
+        : L.circleMarker(latLng, {
+              radius: 1
+          });
 };
 
-const geoJsonData: FeatureCollection[] = coordList.map(coords => getRadarRanges(coords, distance));
+const geoJsonData: FeatureCollection[] = coordList.map((coords) => getRadarRanges(coords, distance));
 
-const geoData = geoJsonData.map(d =>
-    <GeoJSON key={hash(d)} data={d} pointToLayer={pointToLayer}/>)
+const geoData = geoJsonData.map((d) => <GeoJSON key={hash(d)} data={d} pointToLayer={pointToLayer} />);
 
 function App() {
     return (
@@ -104,7 +90,7 @@ function App() {
             />
             {geoData}
         </MapContainer>
-    )
+    );
 }
 
-export default App
+export default App;
